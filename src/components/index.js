8db71c6e04f1584e156cf8eacba8d096be634a39
Talha <wajidtalha9@gmail.com>
2024-10-16 20:56:38 +0500

    all files

---
+++ src/components/index.js
@@ -0,0 +1,22 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+export {
+  Navbar,
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  Stats,
+  Hero,
+  Footer,
+  Testimonials
+};
